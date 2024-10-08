8acf6f987b9b55755fb88dcd62789cd867e9cb32
Sergio Marrero Fernandez <sergio@Sergios-Mac-mini.local>
2024-10-08 16:11:06 +0100

    Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sergio Marrero Fernandez on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
