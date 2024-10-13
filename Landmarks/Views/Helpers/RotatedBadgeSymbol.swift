68dfb7211c77de43b77dedc0184d243b148795cb
Sergio Marrero Fernandez <sergio@Sergios-Mac-mini.local>
2024-10-13 18:44:08 +0100

    Drawing paths and shapes

---
+++ Landmarks/Views/Helpers/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Sergio Marrero Fernandez on 13/10/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
